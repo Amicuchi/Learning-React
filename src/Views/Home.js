7eb5d63c77b10ca0442e55f3bcc8ead29df187a1
Anderson Amicuchi Machado <andersonamicuchi@gmail.com>
2024-06-16 18:15:05 -0300

    Criação de um hook para diminuir a quantidade de código e facilitar  a manutenibilidade.

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,41 +1,44 @@
-import React, {useEffect, useState} from "react";
-import axios from "axios";
+import React  from "react";
 import Loader from "../Components/Loader";
 import ProductCard from "../Components/ProductCard";
+import useAxiosGet from "../Hooks/HttpRequests";
 
 function Home() {
     const url = `https://666f2a3ef1e1da2be52227f8.mockapi.io/products/product?page=1&limit=10`;
-    const [products, setProducts] = useState({
-        loading: false,
-        data: null,
-        error: false
-    });
+    // const [products, setProducts] = useState({
+    //     loading: false,
+    //     data: null,
+    //     error: false
+    // });
+
+    let products = useAxiosGet(url);
+
     let content = null;
 
-    useEffect( () => {
-        setProducts({
-            loading: true,
-            data: null,
-            error: false,
-        });
+    // useEffect( () => {
+    //     setProducts({
+    //         loading: true,
+    //         data: null,
+    //         error: false,
+    //     });
 
-        axios
-            .get(url)
-            .then(response => {
-                setProducts({
-                    loading: false,
-                    data: response.data,
-                    error: false
-                })
-            })
-            .catch( () => {
-                setProducts({
-                    loading: false,
-                    data: null,
-                    error: true
-                })
-            });
-    }, [url]);
+    //     axios
+    //         .get(url)
+    //         .then(response => {
+    //             setProducts({
+    //                 loading: false,
+    //                 data: response.data,
+    //                 error: false
+    //             })
+    //         })
+    //         .catch( () => {
+    //             setProducts({
+    //                 loading: false,
+    //                 data: null,
+    //                 error: true
+    //             })
+    //         });
+    // }, [url]);
 
     if(products.error){
         content = <p>There was an error. Please, try again.</p>
--- src/Views/Product.js
+++ src/Views/Product.js
@@ -1,42 +1,45 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import { useParams } from "react-router-dom";
-import axios from "axios";
 import Loader from "../Components/Loader";
+import useAxiosGet from "../Hooks/HttpRequests";
 
 function Product() {
     const { id } = useParams()
     const url = `https://666f2a3ef1e1da2be52227f8.mockapi.io/products/product/${id}`;
-    const [product, setProduct] = useState({
-        loading: false,
-        data: null,
-        error: false
-    });
+    // const [product, setProduct] = useState({
+    //     loading: false,
+    //     data: null,
+    //     error: false
+    // });
+
+    let product = useAxiosGet(url);
+
     let content = null;
 
-    useEffect( () => {
-        setProduct({
-            loading: true,
-            data: null,
-            error: false,
-        });
+    // useEffect( () => {
+    //     setProduct({
+    //         loading: true,
+    //         data: null,
+    //         error: false,
+    //     });
 
-        axios
-            .get(url)
-            .then(response => {
-                setProduct({
-                    loading: false,
-                    data: response.data,
-                    error: false
-                })
-            })
-            .catch( () => {
-                setProduct({
-                    loading: false,
-                    data: null,
-                    error: true
-                })
-            });
-    }, [url]);
+    //     axios
+    //         .get(url)
+    //         .then(response => {
+    //             setProduct({
+    //                 loading: false,
+    //                 data: response.data,
+    //                 error: false
+    //             })
+    //         })
+    //         .catch( () => {
+    //             setProduct({
+    //                 loading: false,
+    //                 data: null,
+    //                 error: true
+    //             })
+    //         });
+    // }, [url]);
 
     if(product.error){
         content = <p>There was an error. Please, try again.</p>
