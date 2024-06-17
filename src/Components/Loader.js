c68670126f27aa870f756997f60ba9d1eb297818
Anderson Amicuchi Machado <andersonamicuchi@gmail.com>
2024-06-16 21:58:16 -0300

    Exclusão da folha de estilo e criação dos StyledComponents para suprir essa necessidade.

--- src/Components/Loader.js
+++ src/Components/Loader.js
@@ -1,9 +1,24 @@
+// Esse componente serve apenas para mostrar ao usuário que a requisição dele foi realizada e que será atendida.
+// Por mera conveniência, o componente foi configurado visualmente aqui mesmo, mas poderia ter sido feito num arquivo separado.
+
 import React from "react";
+import styled from 'styled-components'
+
+const LoaderDevice = styled.div`
+    border: 5px solid #d1d1d1;
+    border-radius: 50%;
+    border-top: 5px solid #3498db;
+    width: 50px;
+    height: 50px;
+    -webkit-animation: spin 2s linear infinite;
+    animation: spin 2s linear infinite;
+`;
 
 function Loader() {
     return(
         <>
-            <div className="loader"></div>
+            {/* <div className="loader"></div> */}
+            <LoaderDevice />
         </>
     );
 };
--- src/Components/ProductCard.js
+++ src/Components/ProductCard.js
@@ -1,16 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from 'styled-components'
+
+const ProductCardStyle = styled.div`
+    width: 300px;
+    border: 1px solid #3498db;
+    border-radius: 10px;
+    padding: 20px;
+    margin-bottom: 20px;
+    overflow: hidden;
+    align-items: center;
+    text-align:center;
+`;
+
+const ProductCardImgStyled = styled.img`
+    width: 250px;
+    height: 250px;
+    border-radius: 10px;
+`;
 
 function ProductCard(props) {
     return(
-        <div className="productCard"> 
+        <ProductCardStyle> 
             <Link to={`/product/${props.product.id}`}>
-                <img src={props.product.images} alt={props.product.name} />
+                <ProductCardImgStyled src={props.product.images} alt={props.product.name} />
             </Link>
                 <h3>{props.product.name}</h3> 
                 <h4>$ {props.product.price}</h4>
                 <p>{props.product.description}</p>
-        </div>
+        </ProductCardStyle>
     )
 }
 
