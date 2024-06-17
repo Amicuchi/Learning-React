4f3e0d6ca5d10ed166f1315fd6380f7ce6ca02d0
Anderson Amicuchi Machado <andersonamicuchi@gmail.com>
2024-06-16 22:03:33 -0300

    Exclusão de código legado que já não era mais utilizado.

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -5,40 +5,8 @@ import useAxiosGet from "../Hooks/HttpRequests";
 
 function Home() {
     const url = `https://666f2a3ef1e1da2be52227f8.mockapi.io/products/product?page=1&limit=10`;
-    // const [products, setProducts] = useState({
-    //     loading: false,
-    //     data: null,
-    //     error: false
-    // });
-
     let products = useAxiosGet(url);
-
     let content = null;
-
-    // useEffect( () => {
-    //     setProducts({
-    //         loading: true,
-    //         data: null,
-    //         error: false,
-    //     });
-
-    //     axios
-    //         .get(url)
-    //         .then(response => {
-    //             setProducts({
-    //                 loading: false,
-    //                 data: response.data,
-    //                 error: false
-    //             })
-    //         })
-    //         .catch( () => {
-    //             setProducts({
-    //                 loading: false,
-    //                 data: null,
-    //                 error: true
-    //             })
-    //         });
-    // }, [url]);
 
     if(products.error){
         content = <p>There was an error. Please, try again.</p>
--- src/Views/Product.js
+++ src/Views/Product.js
@@ -6,40 +6,8 @@ import useAxiosGet from "../Hooks/HttpRequests";
 function Product() {
     const { id } = useParams()
     const url = `https://666f2a3ef1e1da2be52227f8.mockapi.io/products/product/${id}`;
-    // const [product, setProduct] = useState({
-    //     loading: false,
-    //     data: null,
-    //     error: false
-    // });
-
     let product = useAxiosGet(url);
-
     let content = null;
-
-    // useEffect( () => {
-    //     setProduct({
-    //         loading: true,
-    //         data: null,
-    //         error: false,
-    //     });
-
-    //     axios
-    //         .get(url)
-    //         .then(response => {
-    //             setProduct({
-    //                 loading: false,
-    //                 data: response.data,
-    //                 error: false
-    //             })
-    //         })
-    //         .catch( () => {
-    //             setProduct({
-    //                 loading: false,
-    //                 data: null,
-    //                 error: true
-    //             })
-    //         });
-    // }, [url]);
 
     if(product.error){
         content = <p>There was an error. Please, try again.</p>
